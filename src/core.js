f5600b5dd13082084abb33b76abe7fa6d084912b
Daniel <truenorthtrainingcompany@gmail.com>
2016-11-15 21:57:59 -0500

    Define reducer Tests for use cases

--- src/core.js
+++ src/core.js
@@ -1,33 +1,9 @@
 import {List} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
 	return state.set('entries', entries);
-}
-
-export function next(state) {
-	const entries = state.get('entries');
-	return state.merge({
-		vote: Map({pair: entries.take(2)}),
-		entries: entries.skip(2)
-	});
-}
-
-export function vote(state, entry) {
-	return state.updateIn(
-		['vote', 'tally', entry],
-		0,
-		tally => tally + 1
-	);
-}
-
-function getWinners(vote) {
-	if (!vote) return [];
-	const [a, b] = vote.get('pair');
-	const aVotes = vote.getIn(['tally', a], 0);
-	const bVotes = vote.getIn(['tally', b], 0);
-	if 			(aVotes > bVotes) return [a];
-	else if (aVotes < bVotes) return [b];
-	else 											return [a, b];
 }
 
 export function next(state) {
@@ -42,4 +18,22 @@ export function next(state) {
 			entries: entries.skip(2)
 		});
 	}
+}
+
+export function vote(state, entry) {
+	return state.updateIn(
+		['vote', 'tally', entry],
+		0,
+		tally => tally + 1
+	);
+}
+
+export function getWinners(vote) {
+	if (!vote) return [];
+	const [a, b] = vote.get('pair');
+	const aVotes = vote.getIn(['tally', a], 0);
+	const bVotes = vote.getIn(['tally', b], 0);
+	if 			(aVotes > bVotes) return [a];
+	else if (aVotes < bVotes) return [b];
+	else 											return [a, b];
 }
