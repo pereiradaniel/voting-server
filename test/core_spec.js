2511c16eeee37fd082acbacd486b4e5e90526931
Daniel <truenorthtrainingcompany@gmail.com>
2016-11-15 22:05:38 -0500

    Remove duplicate line

--- test/core_spec.js
+++ test/core_spec.js
@@ -2,8 +2,6 @@ import {List, Map} from 'immutable';
 import {expect} from 'chai';
 import {setEntries, next} from '../src/core';
 
-import {setEntries} from '../src/core';
-
 describe('application logic', () => {
 
 	describe('setEntries', () => {
